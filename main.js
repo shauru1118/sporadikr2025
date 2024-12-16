fbaca16e25a22cf46f69e1a00b7aac8a0af273b3
Albom3 <sargsyan_ae09@mail.ru>
2024-12-16 22:58:03 +0300

    2257

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-console.log("strart...\n\n");
+console.warn("Starting");
 // ====================
 
-const name = "Albert"
+const name = "Albert";
 const header = document.querySelector("#header");
-const links = document.querySelectorAll(".link")
-
-console.log(links);
-console.log(header);
+const links = document.querySelectorAll(".link");
 
 
 for (let item of links) {
     item.addEventListener("click", function() {
         console.log(item.text);
     });
+    console.log("done - " + item.text);
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log(i + ". Hi");
 }
 
 
-// ====================
-console.log("\nend...\n");
+console.warn("Start has done!")
+
