5382966cd7f977ff7c2ee9a699c6f4c2238912d7
Albom3 <sargsyan_ae09@mail.ru>
2024-12-17 14:40:54 +0300

    1440

--- main.js
+++ main.js
@@ -4,18 +4,19 @@ console.warn("Starting");
 const name = "Albert";
 const header = document.querySelector("#header");
 const links = document.querySelectorAll(".link");
-
+// const pressBtn = document.querySelector("#press-btn");
 
 for (let item of links) {
     item.addEventListener("click", function() {
         console.log(item.text);
+        alert("reflex");
     });
     console.log("done - " + item.text);
 }
 
-for (var i = 0; i < 10; i++) {
-    console.log(i + ". Hi");
-}
+// pressBtn.addEventListener('click', function() {
+//     alert("Hi!!!")
+// });
 
 
 console.warn("Start has done!")
