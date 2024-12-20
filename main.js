257eda1e97293af203f4eee34291b85efb342f26
Albom3 <sargsyan_ae09@mail.ru>
2024-12-20 22:53:06 +0300

    2253

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.warn("Starting");
 // ====================
 
-const name = "Albert";
+var name = "Unknown";
 const header = document.querySelector("#header");
 const links = document.querySelectorAll(".link");
 // const pressBtn = document.querySelector("#press-btn");
@@ -9,10 +9,15 @@ const links = document.querySelectorAll(".link");
 for (let item of links) {
     item.addEventListener("click", function() {
         console.log(item.text);
-        alert("reflex");
+        // alert("reflex");
     });
     console.log("done - " + item.text);
 }
+
+// var name = prompt("What is your name?");
+document.querySelector("#user-link").innerHTML = name;
+// prompt();
+
 
 // pressBtn.addEventListener('click', function() {
 //     alert("Hi!!!")
