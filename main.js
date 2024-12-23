2bffe28d263798632c483f3505a564bbacb2557c
Albom3 <sargsyan_ae09@mail.ru>
2024-12-23 14:58:03 +0300

    1457

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ console.warn("Starting");
 var name = "Unknown";
 const header = document.querySelector("#header");
 const links = document.querySelectorAll(".link");
+const calcBtn = document.querySelector("#calcbtn");
+const calcRes = document.querySelector("#calcresult");
+const inputNum1 = document.querySelector("#num1");
+const inputNum2 = document.querySelector("#num2");
 // const pressBtn = document.querySelector("#press-btn");
 
 for (let item of links) {
@@ -13,6 +17,14 @@ for (let item of links) {
     });
     console.log("done - " + item.text);
 }
+
+calcBtn.addEventListener("click", function() {
+    let num1 = inputNum1.value;
+    let num2 = inputNum2.value;
+    calcRes.innerHTML = parseFloat(num1) + parseFloat(num2);
+    console.log("Calculated!")
+});
+
 
 // if (name == "Unknown") {
 //     var name = prompt("What is your name?");
