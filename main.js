9f6192a62767903f4d11ab5d2c6f67a8a5564996
Albom3 <sargsyan_ae09@mail.ru>
2025-02-19 21:41:24 +0300

    new

--- main.js
+++ main.js
@@ -1,44 +1,11 @@
-console.warn("Starting");
-// ====================
+let presbut = document.querySelector("#pressbut")
 
-var name = "Unknown";
-const header = document.querySelector("#header");
-const links = document.querySelectorAll(".link");
-const calcBtn = document.querySelector("#calcbtn");
-const calcRes = document.querySelector("#calcresult");
-const inputNum1 = document.querySelector("#num1");
-const inputNum2 = document.querySelector("#num2");
-// const pressBtn = document.querySelector("#press-btn");
+presbut.addEventListener('click', loginbut)
 
-for (let item of links) {
-    item.addEventListener("click", function() {
-        console.log(item.text);
-        // alert("reflex");
-    });
-    console.log("done - " + item.text);
+function loginbut() {
+    let l = document.getElementById("login-input").value
+    let p = document.getElementById("password-input").value
+
+    alert("Login_______:    " + l + "\nPassword___:    " + p + "\n")
+
 }
-
-calcBtn.addEventListener("click", function() {
-    let num1 = inputNum1.value;
-    let num2 = inputNum2.value;
-    calcRes.innerHTML = parseFloat(num1) + parseFloat(num2);
-    console.log("Calculated!")
-});
-
-
-// if (name == "Unknown") {
-//     var name = prompt("What is your name?");
-// }
-
-// document.querySelector("#user-link").innerHTML = name;
-// document.querySelector("#user_name").innerHTML = name;
-// prompt();
-
-
-// pressBtn.addEventListener('click', function() {
-//     alert("Hi!!!")
-// });
-
-
-console.warn("Start has done!")
-
